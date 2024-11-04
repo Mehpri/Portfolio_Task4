f798a799f7647c41fee62109b61316690b8a108b
Malachi <gqxd5106@leeds.ac.uk>
2024-11-04 02:35:06 +0000

    made it better

--- Currency_exchange.py
+++ Currency_exchange.py
@@ -5,9 +5,13 @@ def currency_converter(amount,from_currency,to_currency):
 'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
 'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
 }
-    rate = (conversion_rate[from_currency][to_currency])
-    converted = round((amount * rate),2)
-    print(converted)
+    if amount < 0:
+        converted = 0.0
+        print(converted)
+    else:
+        rate = (conversion_rate[from_currency][to_currency])
+        converted = round((amount * rate),2)
+        print(converted)
 
 amount = int(input("Enter in the numerical amount you want converting:"))
 from_currency, to_currency = input("Enter the currency code for the currency you're converting from and what you want to convert to:").split()
